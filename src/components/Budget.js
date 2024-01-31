a0e580de1ecc1179a2458befe7d2d964983edb5b
SAGAWARctor <134320038+SAGAWARctor@users.noreply.github.com>
2024-01-31 15:10:03 -0500

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,26 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+	const { budget } = useContext(AppContext);
+	const [newBudget, setNewBudget] = useState(budget);
+	const [error, setError] = useState('');
+
+	const handleBudgetChange = (event) => {
+		const value = event.target.value;
+		if (value > 20000) {
+			setError('Cannot exceed 20,000');}
+        else if (value < budget) {
+            setError('Budget cannot be lower than the initial budget');}
+		else {
+			setError('');
+			setNewBudget(value);}
+		}
+
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+{error && <div style={{ color: 'red' }}>{error}</div>}
 </div>
     );
 };
